22808aa84b592c66627ca954e061ea65924c21f6
SimoCrash <simone.crescenzi@hotmail.com>
2022-11-09 17:50:17 +0100

    icone fatte

--- js/data.js
+++ js/data.js
@@ -3,112 +3,112 @@ const arrCard = [
 		name: 'cat',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
-		family: 'fas',
+		family: 'fa',
 		color: 'orange'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa',
 		color: 'green'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa',
 		color: 'green'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa',
 		color: 'green'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
-		family: 'fas',
+		family: 'fa',
 		color: 'green'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa',
 		color: 'blue'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa',
 		color: 'blue'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa',
 		color: 'blue'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
-		family: 'fas',
+		family: 'fa',
 		color: 'blue'
 	}
 ];
@@ -122,10 +122,10 @@ const arrCard = [
 arrCard.forEach((element) => {
 	const eleCard = document.querySelector(".cards");
 	let card = document.createElement("div");
-	// card = document.classList.add("card");
+	card.classList.add("card");
 	eleCard.append(card);
 	card.innerHTML = `
-	<i class=" ${element.family} ${element.prefix}${element.name}"></i>
+	<i class="${element.family}-solid ${element.prefix}${element.name}"></i>
 	<div> ${element.name} </div>
 	`
 	
